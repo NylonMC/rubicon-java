01c8a1a8ff83781270bde2f89122634eeade7669
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-08-24 14:14:42 -0500

    ahh

--- rubicon/java/__init__.py
+++ rubicon/java/__init__.py
@@ -1,6 +1,6 @@
 from .api import *   # noqa; F401, F403
 from .jni import *   # noqa; F401, F403
 from .types import *  # noqa; F401, F403
-import .stdout_hack
+from .stdout_hack import *
 
 __version__ = '0.2.4'
