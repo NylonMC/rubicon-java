64578fb5904ccceab93e3c359e5be523e3dc7fd4
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-08-24 14:35:18 -0500

    ahhhhhhhhhhhhh

--- rubicon/java/stout_hack.py
+++ rubicon/java/stout_hack.py
@@ -1,21 +1,25 @@
 from . import JavaClass
-import io
-import sys
+
 
 _log = JavaClass("org/apache/logging/log4j/LogManager").getLogger("Python")
+try:
+    import io
+    import sys
 
-class _StdoutWrapper(io.StringIO):
-    def flush(self):
-        for line in self.getvalue().split("\n"):
-            if line != "":
-                _log.info(line)
-                # self.truncate(0)
-sys.stdout = _StdoutWrapper()
+    class _StdoutWrapper(io.StringIO):
+        def flush(self):
+            for line in self.getvalue().split("\n"):
+                if line != "":
+                    _log.info(line)
+                    # self.truncate(0)
+    sys.stdout = _StdoutWrapper()
 
-class _StderrWrapper(io.StringIO):
-    def flush(self):
-        for line in self.getvalue().split("\n"):
-            if line != "":
-                _log.error(line)
-                # self.truncate(0)
-sys.stderr = _StderrWrapper()
+    class _StderrWrapper(io.StringIO):
+        def flush(self):
+            for line in self.getvalue().split("\n"):
+                if line != "":
+                    _log.error(line)
+                    # self.truncate(0)
+    sys.stderr = _StderrWrapper()
+except Exception as e:
+    _log.error(str(e))
